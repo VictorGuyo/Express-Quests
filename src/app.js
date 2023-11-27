b0439261ebb65295f3778a6065b181d8956033a6
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-11-27 17:31:32 +0100

    Quest 1.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 
+const port = process.env.APP_PORT;
 const app = express();
 
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", movieControllers.getMovies);
+app.get("/api/users/:id", movieControllers.getMovieById);
 
 module.exports = app;
