81d52a22c969a61399b854bb8d384d07eecfc654
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-11-28 09:45:25 +0100

    Quest 1.2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,9 @@ const movieControllers = require("./controllers/movieControllers");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
-app.get("/api/users", movieControllers.getMovies);
-app.get("/api/users/:id", movieControllers.getMovieById);
+const usersControllers = require("./controllers/usersControllers"); 
+
+app.get("/api/users", usersControllers.getUsers);
+app.get("/api/users/:id", usersControllers.getUsersById);
 
 module.exports = app;
